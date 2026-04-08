d30fe5648f46208825392574b3446c30c184d9bb
MLGalusha <masonlgalusha@gmail.com>
2026-04-08 09:54:34 -0400

    Fix barrel export collision between queue and processes APIs

--- frontend/src/api/admin/index.ts
+++ frontend/src/api/admin/index.ts
@@ -4,7 +4,32 @@ export * from "./versions";
 export * from "./bulk";
 export * from "./processing";
 export * from "./queue";
-export * from "./processes";
+export {
+  getAllProcessesStatus,
+  getProcessEligibility,
+  getProcessQueue,
+  getProcessRecent,
+  startProcess,
+  pauseBatch,
+  resumeBatch,
+  abortBatch,
+  removeFromProcessQueue,
+  clearProcessQueue,
+  retryProcessJob,
+  cancelActiveJob as cancelProcessActiveJob,
+  getProcessingStreamToken,
+  type ProcessKey,
+  type ProcessCapabilities,
+  type ProcessStatus,
+  type QueuedItem as ProcessQueuedItem,
+  type ActiveJob,
+  type RecentJob,
+  type ObservedWorkerState,
+  type ActiveBatchState,
+  type AllProcessesStatus,
+  type ProcessFilters,
+  type ProcessingEvent,
+} from "./processes";
 export * from "./extras";
 export * from "./photoDescriptions";
 export * from "./transcription";
